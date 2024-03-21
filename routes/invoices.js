5c8ae30355a03ef878fe84c0fe8fe177165bdaa5
Cooper Spinelli <cooperspinelli@gmail.com>
2024-03-21 16:31:57 -0600

    Add routes for editing and creating invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = new express.Router();
 
 const db = require("../db");
-const { NotFoundError } = require('../expressError');
+const { NotFoundError, BadRequestError } = require('../expressError');
 
 /** Returns json containing all invoices
  * return json with like {invoices: [{id, comp_code}, ...]}
@@ -44,6 +44,65 @@ router.get('/:id', async function (req, res, next) {
 
   invoice.company = company;
   delete invoice.comp_code;
+  return res.json({ invoice });
+});
+
+
+/**
+ * Adds an invoice to the database:
+ * Expects {comp_code, amt}
+ * Returns {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+ */
+router.post('/', async function (req, res, next) {
+  const { comp_code, amt } = req.body;
+
+  if (!(comp_code && (amt !== undefined))) {
+    throw new BadRequestError();
+  }
+
+  let result;
+  try {
+    result = await db.query(
+      `INSERT INTO invoices (comp_code, amt)
+      VALUES ($1, $2)
+      RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+      [comp_code, Number(amt)]);
+  } catch {
+    throw new BadRequestError();
+  }
+
+  const invoice = result.rows[0];
+
+  return res.json({ invoice });
+});
+
+
+/**
+ * Updates an invoice in the database:
+ * Expects {amt}
+ * Returns {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+ */
+router.put('/:id', async function (req, res, next) {
+  const invoiceId = req.params.id;
+  const { amt } = req.body;
+
+  if (amt === undefined) {
+    throw new BadRequestError();
+  }
+
+  const result = await db.query(
+    `UPDATE invoices
+      SET amt=$1
+      WHERE id=$2
+      RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+    [Number(amt), invoiceId]);
+
+  const invoice = result.rows[0];
+
+  if (!invoice) {
+    throw new NotFoundError();
+  }
+
   return res.json({ invoice });
 });
 
